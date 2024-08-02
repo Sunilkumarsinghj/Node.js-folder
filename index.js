b5560db4109bf0b1cf375500c70181367c92b46d
sunilkumarsinghj <sunilworld1234ss@gmail.com>
2024-08-02 17:22:57 +0530

    pushed

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
- let colors=require('colors');
- console.log("package.json".bgGreen)
- console.log(colors.green('hello'))
+
+// let chalk = require("chalk");
+let colors=require('colors');
+ console.log("package.json".bgGreen);
+ console.log(colors.green('hello')) ;
+ console.log("hello")
+
+//  console.log(chalk.blue('Hello world!'));
